3e8ab006e364e94747499e2cb4c119a7a1efebd8
francesco madonna <fra.mad.93@hotmail.it>
2024-05-19 14:11:14 +0200

    aggiunta risposta e ricerca contatti

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ createApp({
        
         return{
             activeChat: 0,
+            newMessage: "",
+            newReply: "",
+            newSearch: "",
             contacts: [
                 {
                     name: 'Michele',
@@ -12,17 +15,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2024 15:30:55',
                             message: 'Hai portato a spasso il cane?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2024 15:50:00',
                             message: 'Ricordati di stendere i panni',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 16:15:22',
+                            date: '10/01/2024 16:15:22',
                             message: 'Tutto fatto!',
                             status: 'received'
                         }
@@ -34,17 +37,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '20/03/2020 16:30:00',
+                            date: '20/03/2024 16:30:00',
                             message: 'Ciao come stai?',
                             status: 'sent'
                         },
                         {
-                            date: '20/03/2020 16:30:55',
+                            date: '20/03/2024 16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received'
                         },
                         {
-                            date: '20/03/2020 16:35:00',
+                            date: '20/03/2024 16:35:00',
                             message: 'devo andare a fare la spesa.',
                             status: 'sent'
                         }
@@ -56,17 +59,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '28/03/2020 10:10:40',
+                            date: '28/03/2024 10:10:40',
                             message: 'La Marianna va in campagna',
                             status: 'received'
                         },
                         {
-                            date: '28/03/2020 10:20:10',
+                            date: '28/03/2024 10:20:10',
                             message: 'Sicuro di non aver sbagliato chat?',
                             status: 'sent'
                         },
                         {
-                            date: '28/03/2020 16:15:22',
+                            date: '28/03/2024 16:15:22',
                             message: 'Ah scusa!',
                             status: 'received'
                         }
@@ -78,12 +81,12 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2024 15:30:55',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2024 15:50:00',
                             message: 'Si, ma preferirei andare al cinema',
                             status: 'received'
                         }
@@ -95,12 +98,12 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2024 15:30:55',
                             message: 'Ricordati di chiamare la nonna',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2024 15:50:00',
                             message: 'Va bene, stasera la sento',
                             status: 'received'
                         }
@@ -112,17 +115,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2024 15:30:55',
                             message: 'Ciao Claudia, hai novità?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2024 15:50:00',
                             message: 'Non ancora',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '10/01/2024 15:51:00',
                             message: 'Nessuna nuova, buona nuova',
                             status: 'sent'
                         }
@@ -134,12 +137,12 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2024 15:30:55',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2024 15:50:00',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
                             status: 'received'
                         }
@@ -151,17 +154,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2024 15:30:55',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2024 15:50:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '10/01/2024 15:51:00',
                             message: 'OK!!',
                             status: 'received'
                         }
@@ -172,11 +175,41 @@ createApp({
        
         
     },
-
+    
     methods: {
         clickContact(index){
             this.activeChat = index;
         },
+
+        addMessage(){
+            const nMsg = {            
+                message: this.newMessage,
+                status: 'sent'
+            }
+            if (this.newMessage.trim() !== "") {
+                this.contacts[this.activeChat].messages.push(nMsg);
+                this.newMessage = "";
+                this.newReply = setTimeout(this.addReply, 1000);
+            }
+        },
+        
+        addReply(){
+            const newMsg = {               
+                message: "ok",
+                status: 'received'
+            }
+            this.contacts[this.activeChat].messages.push(newMsg);
+        },
+
+        findFriend() {
+            this.contacts.forEach(contact => {
+                const contactName = contact.name.toLowerCase();
+                const search = this.newSearch.toLowerCase();
+                contact.visible = contactName.includes(search);
+            });
+        },
+        
+    },
        
-    }
 }).mount('#app')
+    
